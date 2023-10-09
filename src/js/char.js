6821b4ec8b017c7eed38e0a66f68d2ff3740e1b3
Sergei Snitko <sergeisnit@yandex.ru>
2023-10-09 20:54:44 +0300

    Версия 2. Разбивка на description и остальное

--- src/js/char.js
+++ src/js/char.js
@@ -1,10 +1,17 @@
 export default function getSpecialAbility(char) {
   const { special } = char;
+  const abilities = [];
   special.forEach((element) => {
-    if (!Object.prototype.hasOwnProperty.call(element, 'description')) {
-      const addDescription = { description: 'Описание недоступно' };
-      Object.assign(element, addDescription);
-    }
+    // if (!Object.prototype.hasOwnProperty.call(element, 'description')) {
+    //   const addDescription = { description: 'Описание недоступно' };
+    //   Object.assign(element, addDescription);
+    // }
+    // console.log(element);
+    // const { description = 'Описание недоступно' } = element;
+    const { description = 'Описание недоступно', ...params } = element;
+
+    Object.assign(element, { description: description });
+    // console.log(element);
   });
   return special;
 }
