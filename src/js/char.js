467d35bb2acb516ae9f81d164c594fb0e41b42f4
Sergei Snitko <sergeisnit@yandex.ru>
2023-10-09 21:15:46 +0300

    Версия 3. Объект в качестве аргумента .forEach({})

--- src/js/char.js
+++ src/js/char.js
@@ -1,17 +1,9 @@
 export default function getSpecialAbility(char) {
   const { special } = char;
   const abilities = [];
-  special.forEach((element) => {
-    // if (!Object.prototype.hasOwnProperty.call(element, 'description')) {
-    //   const addDescription = { description: 'Описание недоступно' };
-    //   Object.assign(element, addDescription);
-    // }
-    // console.log(element);
-    // const { description = 'Описание недоступно' } = element;
-    const { description = 'Описание недоступно', ...params } = element;
-
-    Object.assign(element, { description: description });
-    // console.log(element);
+  special.forEach(({ description = 'Описание недоступно', ...pars }) => {
+    const ability = { ...pars, description };
+    abilities.push(ability);
   });
-  return special;
+  return abilities;
 }
